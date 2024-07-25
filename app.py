9eb5980489f5a2f56964c1c36c14f2e50a5ea00f
EstellePMV <estelle.colin@primever.com>
2024-07-25 16:52:52 +0200

    add function for compare solution and balloons when correct

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # pylint: disable = missing-module-docstring
-
+import datetime
 import os
 import logging
 import duckdb
 import streamlit as st
+from datetime import date, timedelta
 
 if "data" not in os.listdir():
     logging.error(os.listdir())
@@ -15,6 +16,30 @@ if "exercises_sql_tables.duckdb" not in os.listdir("data"):
     # subprocess.run(["python', "init_db.py"])
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
+
+def check_users_solution(user_query: str) -> None:
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    result = con.execute(user_query).df()
+    st.dataframe(result)
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
+    except KeyError as e:
+        st.write("Some columns are missing")
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has {n_lines_difference} lines differences with the solution_df"
+        )
 
 
 with st.sidebar:
@@ -44,25 +69,22 @@ with st.sidebar:
 
     solution_df = con.execute(answer).df()
 
-
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
+
 if query:
-    result = con.execute(query).df()
-    st.dataframe(result)
+    check_users_solution(query)
 
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
 
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"result has {n_lines_difference} lines differences with the solution_df"
-        )
+for n_days in [2, 7, 21]:
+    if st.button(f'Revoir dans {n_days} jours'):
+        new_date = date.today() + timedelta(days=n_days)
+        con.execute(f"UPDATE memory_state SET last_reviewed = '{new_date}' WHERE exercise_name = '{exercise_name}'")
+        st.rerun()
 
+if st.button('Reset'):
+    con.execute(f"UPDATE memory_state SET last_reviewed = '1970-01-01'")
+    st.rerun()
 
 tab1, tab2 = st.tabs(["Tables", "Solution"])
 #
